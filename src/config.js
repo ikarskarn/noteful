90cc2a274cf69d78108a40d3791249e98093206e
Marc Tucker <marcttucker@gmail.com>
2020-09-27 22:10:40 -0400

    added vercel code and config files

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export default {
+	API_ENDPOINT: `postgres://pqrjulllvshazn:c790c27ecc51e3331521821ada95e18341ad2a8f354a32621161f02dc5b880ce@ec2-52-86-116-94.compute-1.amazonaws.com:5432/d1cbofu3q1rqqk`,
+	API_KEY: process.env.REACT_APP_API_KEY,
+}
